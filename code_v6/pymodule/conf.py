e028a58de683efe5e86751b6385b7a7f44e1f989
lacus577 <jiatao577@163.com>
2020-06-03 11:02:20 +0800

    init

---
+++ code_v6/pymodule/conf.py
@@ -0,0 +1,64 @@
+''''''
+''' -------------------------- conf --------------------------- '''
+''' 进程并发数 '''
+process_num = 3
+
+now_phase = 6
+
+
+''' 样本采样个数 '''
+subsampling = 3
+
+''' 全量点击序 '''
+is_click_cached = True
+
+''' 训练集、测试集 '''
+''' 训练集、测试集样本是否已经缓存 '''
+is_samples_cached = True
+''' 训练集、测试集特征是否已经缓存 '''
+is_feature_cached = False
+
+''' 召回结果 '''
+''' 召回结果缓存 '''
+is_recall_cached = True
+''' 召回结果样本缓存 '''
+is_recall_sample_cached = False
+''' 召回结果特征缓存 '''
+is_recall_feature_cached = False
+
+''' itemcf相似度前k个'''
+itemcf_num = 5
+
+''' 负样本数量 '''
+negative_num = 100
+#
+# ''' 召回数量 '''
+# recall_num = 500
+
+
+''' -------------------------- constant --------------------------- '''
+train_path = '../../../../data/underexpose_train'
+test_path = '../../../../data/underexpose_test'
+
+''' 全量点击序 '''
+click_cache_path = './cache/features_cache/click.csv'
+
+''' 召回结果缓存路径 '''
+# recall_cache_path = './cache/features_cache/test_user_recall_{}.csv'
+recall_cache_path = './cache/features_cache/phase_{}_recall_500.csv'
+recall_sample_path = './cache/features_cache/test_user_recall_sample_{}.csv'
+recall_feature_path = './cache/features_cache/test_user_recall_feature_{}.csv'
+
+''' 缓存路径合集 '''
+samples_cache_path = './cache/features_cache/samples.csv'
+features_cache_path = './cache/features_cache/features.csv'
+
+''' 训练集user召回结果缓存 '''
+total_user_recall_path = './cache/features_cache/total_user_recall_500.csv'
+
+# 用户点击最大深度
+MAX_CLICK_LEN = 500
+
+# 召回结果中相似度列名
+# ITEM_CF_SCORE = 'itemcf_score'
+ITEM_CF_SCORE = 'sim'
---
+++ code_v6/pymodule/utils.py
@@ -0,0 +1,180 @@
+import os
+from tqdm import tqdm
+
+import pandas as pd
+import numpy as np
+
+def sampling_negtive_samples(user_set, negtive_df, sample_num=10):
+    features = pd.DataFrame()
+    for user in user_set:
+        # TODO 采样比例调优
+        # temp = negtive_df[negtive_df['user_id'] == user]  # 第一版错误，使用了user的历时点击item作为负样本
+        temp = negtive_df[negtive_df['user_id'] != user]    # 第二版：使用全量点击序中非本user的item作为负样本
+        sample_n = temp.shape[0] if temp.shape[0] < sample_num else sample_num
+        temp = temp.sample(n=sample_n, replace=False, random_state=1, axis=0)
+        temp.loc[:, 'user_id'] = user
+        features = features.append(temp)
+
+    return features
+
+def subsampling_user(sample_df, sample_num):
+    # TODO 修改成打散的？
+    sub_user_list = sorted(list(set(sample_df['user_id'])))[:sample_num]
+    sample_df = sample_df[sample_df['user_id'].isin(sub_user_list)]
+
+    # 用户行为序中点击item少于等于2个的，都删除 由于需要将最后一个item作为标签，所以只剩下一个无法构建item相似度矩阵
+    single_item_user_list = list(
+        sample_df['user_id'].value_counts()[sample_df['user_id'].value_counts() <= 2].index)
+    click_train = sample_df[~sample_df['user_id'].isin(single_item_user_list)]
+
+    return click_train
+
+def save(submit_all, topk=None, file_dir=None):
+    import time
+
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    file_name = 'submit{time_str}.csv'.format(time_str=int(time.time()))
+    if file_dir:
+        file_path = os.path.join(file_dir, file_name)
+    else:
+        file_path = os.path.join('./', file_name)
+
+    with open(file_path, 'w') as f:
+        for i, row in submit_all.iterrows():
+            user_id = str(row['user_id'])
+
+            item_list = str(row['item_id']).split(',')
+            if topk:
+                item_list = item_list[:topk]
+
+            if topk:
+                assert len(set(item_list)) == topk
+
+            line = user_id + ',' + ','.join(item_list) + '\n'
+            if topk:
+                assert len(line.strip().split(',')) == (topk + 1)
+
+            f.write(line)
+
+    return file_path
+
+
+def read_train_click(train_path, phase):
+    return pd.read_csv(
+        train_path + '/underexpose_train_click-{phase}.csv'.format(phase=phase)
+        , header=None
+        # , nrows=nrows
+        , names=['user_id', 'item_id', 'time']
+        , sep=','
+        , dtype={'user_id': np.str, 'item_id': np.str, 'time': np.float}
+    )
+
+def read_test_click(test_path, phase):
+    return pd.read_csv(
+        test_path + '/underexpose_test_click-{phase}/underexpose_test_click-{phase}.csv'.format(phase=phase)
+        , header=None
+        # , nrows=nrows
+        , names=['user_id', 'item_id', 'time']
+        , sep=','
+        , dtype={'user_id': np.str, 'item_id': np.str, 'time': np.float}
+    )
+
+def read_qtime(test_path, phase):
+    return pd.read_csv(
+        test_path + '/underexpose_test_click-{phase}/underexpose_test_qtime-{phase}.csv'.format(phase=phase)
+        , header=None
+        # , nrows=nrows
+        , names=['user_id', 'time']
+        , sep=','
+        , dtype={'user_id': np.str, 'time': np.float}
+    )
+
+def two_columns_df2dict(df):
+    result_dict = dict(zip(df.iloc[:, 0], df.iloc[:, 1]))
+    return result_dict
+
+
+def del_qtime_future_click(df):
+    qtime_user_set = set(df[df['train_or_test'] == 'predict']['user_id'])
+
+    result_df = df[~df['user_id'].isin(qtime_user_set)]
+    for qtime_user in tqdm(qtime_user_set):
+        tmp = df[df['user_id'] == qtime_user]
+        tmp = tmp[tmp['time'] <= tmp[tmp['train_or_test'] == 'predict']['time'].iloc[0]]
+
+        result_df = result_df.append(tmp)
+
+    return result_df
+
+
+def del_dup(df):
+    df = df.drop_duplicates(['user_id', 'item_id', 'time'])
+    return df
+
+def process_time(df, time_stamp):
+    df.loc[:, 'time'] = df['time'] * time_stamp
+    return df
+
+def transfer_item_features_df2dict(item_features, dim):
+    txt_vec = item_features.apply(lambda x: np.array(list(x.iloc[-dim-dim: -dim])).reshape(-1, ), axis=1)
+    img_vec = item_features.apply(lambda x: np.array(list(x.iloc[-dim: ])).reshape(-1, ), axis=1)
+    user_key = item_features['item_id']
+
+    assert len(user_key) == len(txt_vec) and len(user_key) == len(img_vec)
+
+    user_features_dict = {}
+    user_features_dict['txt_vec'] = dict(zip(user_key, txt_vec))
+    user_features_dict['img_vec'] = dict(zip(user_key, img_vec))
+
+    return user_features_dict
+
+def transfer_user_features_df2dict(user_features, dim):
+    txt_vec = user_features.apply(lambda x: np.array(list(x.iloc[-dim-dim: -dim])).reshape(-1, ), axis=1)
+    img_vec = user_features.apply(lambda x: np.array(list(x.iloc[-dim: ])).reshape(-1, ), axis=1)
+    user_key = user_features['user_id']
+
+    assert len(user_key) == len(txt_vec) and len(user_key) == len(img_vec)
+
+    user_features_dict = {}
+    user_features_dict['txt_vec'] = dict(zip(user_key, txt_vec))
+    user_features_dict['img_vec'] = dict(zip(user_key, img_vec))
+
+    return user_features_dict
+
+def save_pre_as_submit_format_csv(data_df, out_y):
+    # 构造submit格式csv
+    valid_eval_data = data_df[['user_id', 'item_id']]
+    valid_eval_data['pred_prob'] = out_y
+    valid_eval_data['rank'] = valid_eval_data.groupby(['user_id'])['pred_prob'].rank(ascending=False, method='first')
+    valid_eval_data.sort_values(['rank'], inplace=True)
+
+    valid_submit = valid_eval_data.groupby(['user_id'])['item_id'].agg(lambda x: ','.join(list(x))).reset_index()
+    return valid_submit
+
+
+def get_user2kitem_dict(df, k):
+    df = df.sort_values(['user_id', 'time']).reset_index()
+    df = df.groupby('user_id').head(k)
+    df = df.groupby('user_id').agg({'item_id': lambda x: ','.join(list(x))}).reset_index()
+    df.loc[:, 'item_id'] = df.apply(
+        lambda x: list(x['item_id'].split(',')),
+        axis=1
+    )
+
+    result_dict = dict(zip(df['user_id'], df['item_id']))
+    return result_dict
+
+def get_user2click_span_dict(df):
+    # df = df.sort_values(['user_id', 'time'])
+    df = df.groupby('user_id').agg({'time': lambda x: np.max(list(x)) - np.min(list(x))}).reset_index()
+    result_dict = dict(zip(df['user_id'], df['time']))
+
+    return result_dict
+
+def get_user2total_deg_dict(df):
+    tmp = df.groupby('user_id').agg({'item_deg': lambda x: np.sum(list(x))}).reset_index()
+
+    result_dict = dict(zip(tmp['user_id'], tmp['item_deg']))
+
+    return result_dict
