2a9317dfaf3154defb464ba2e6e002f76b6c1587
lacus577 <jiatao577@163.com>
2020-06-08 17:49:09 +0800

    debug

--- code_v9/pymodule/train.py
+++ code_v9/pymodule/train.py
@@ -62,8 +62,8 @@ if __name__ == '__main__':
     all_phase_click_no_qtime = all_phase_click[all_phase_click['train_or_test'] != 'predict']
 
     # 将数据按天切分成14天，从第七天开始构建样本
-    min_time = int(np.min(all_phase_click_no_qtime[conf.new_time_name]))
-    max_time = int(np.max(all_phase_click_no_qtime[conf.new_time_name])) + 1
+    min_time = int(np.min(all_phase_click_no_qtime['time']))
+    max_time = int(np.max(all_phase_click_no_qtime['time'])) + 1
     step = (max_time - min_time) // conf.days
 
     total_feature_df = pd.DataFrame()
@@ -71,21 +71,21 @@ if __name__ == '__main__':
     for end_time in range(min_time + 7 * step, max_time, step):
         print('period {} ...'.format(end_time))
         period_click_df = all_phase_click_no_qtime[
-            (all_phase_click_no_qtime[conf.new_time_name] <= end_time) &
-            (all_phase_click_no_qtime[conf.new_time_name] >= min_time)
+            (all_phase_click_no_qtime['time'] <= end_time) &
+            (all_phase_click_no_qtime['time'] >= min_time)
         ]
 
         # 对每个user取最后一个点击作为候选正样本，召回命中才是正式正样本
-        period_click_df = period_click_df.sort_values(['user_id', conf.new_time_name]).reset_index(drop=True)
+        period_click_df = period_click_df.sort_values(['user_id', 'time']).reset_index(drop=True)
         candidate_positive_sample_df = period_click_df.groupby('user_id').tail(1).reset_index(drop=True)
         assert candidate_positive_sample_df.shape[0] == len(set(candidate_positive_sample_df['user_id']))
             # 正样本删除，后面要用于构建相似度矩阵
         period_click_df = period_click_df.append(candidate_positive_sample_df)
         period_click_df = \
-            period_click_df.drop_duplicates(['user_id', 'item_id', conf.new_time_name], keep=False)
-        period_click_df = period_click_df.sort_values(['user_id', conf.new_time_name]).reset_index(drop=True)
+            period_click_df.drop_duplicates(['user_id', 'item_id', 'time'], keep=False)
+        period_click_df = period_click_df.sort_values(['user_id', 'time']).reset_index(drop=True)
         assert period_click_df.merge(
-            candidate_positive_sample_df, on=['user_id', 'item_id', conf.new_time_name], how='inner'
+            candidate_positive_sample_df, on=['user_id', 'item_id', 'time'], how='inner'
         ).shape[0] == 0
 
         hot_df = period_click_df.groupby('item_id')['user_id'].count().reset_index()
